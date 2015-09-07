6d131e948a581e26a31cbbd6c9dae7034459a7a0
Shashank Khanna <pitai@users.noreply.github.com>
2015-09-06 19:22:07 -0700

    Update main.js

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ var main  = function(){
       left: "0px"}, 200);
 
     $('body').animate({
-      left: "285px"}, 200);
+      left: "150px"}, 200);
       
   });
   
   $('.icon-close').click(function() {
     $('.menu').animate({
-      left: "-285px"
+      left: "-150px"
     }, 200);
 
     $('body').animate({
